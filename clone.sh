72f11a785c18ab21b4ea589fa50ec094940225c2
BlueHeart01 <sayankumbhakar123@gmail.com>
2026-02-03 23:57:25 -0500

    Add clone.sh script

---
+++ clone.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone -b bka https://github.com/BlueHeart01/redwood_device_xiaomi_redwood.git device/xiaomi/redwood
+
+git clone -b bka https://github.com/BlueHeart01/redwood_vendor_xiaomi_redwood.git vendor/xiaomi/redwood
+
+git clone https://github.com/Redwood-AOSP/android_device_xiaomi_redwood-kernel.git device/xiaomi/redwood-kernel
+
+git clone https://github.com/BlueHeart01/redwood_vendor_xiaomi_redwood-miuicamera.git vendor/xiaomi/redwood-miuicamera
+
+git clone -b moto-1.0 https://github.com/thepriyanshujangid/hardware_dolby.git hardware/dolby
+
+git clone https://github.com/heyharshitrai/hardware_xiaomi.git hardware/xiaomi
