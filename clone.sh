9e0f2323b490bc81372341444392eeee536ba931
Sayan016 <98513272+BlueHeart01@users.noreply.github.com>
2026-02-21 18:43:32 +0530

    Update script

--- clone.sh
+++ clone.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-git clone -b bka https://github.com/BlueHeart01/redwood_device_xiaomi_redwood.git device/xiaomi/redwood
+git clone https://github.com/BlueHeart01/device_xiaomi_redwood.git device/xiaomi/redwood
 
-git clone -b bka https://github.com/BlueHeart01/redwood_vendor_xiaomi_redwood.git vendor/xiaomi/redwood
+git clone https://github.com/BlueHeart01/vendor_xiaomi_redwood.git vendor/xiaomi/redwood
 
 git clone https://github.com/Redwood-AOSP/android_device_xiaomi_redwood-kernel.git device/xiaomi/redwood-kernel
 
 git clone https://github.com/BlueHeart01/redwood_vendor_xiaomi_redwood-miuicamera.git vendor/xiaomi/redwood-miuicamera
 
-git clone -b moto-1.0 https://github.com/thepriyanshujangid/hardware_dolby.git hardware/dolby
+git clone https://github.com/BlueHeart01/vendor_oneplus_dolby.git vendor/oneplus/dolby
 
-git clone https://github.com/heyharshitrai/hardware_xiaomi.git hardware/xiaomi
+git clone https://github.com/BlueHeart01/hardware_xiaomi.git hardware/xiaomi
